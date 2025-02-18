68b66837cebd65af307b8bfd735c7b310e15e888
danieldaniel128 <danielkaarur@gmail.com>
2025-02-18 15:42:45 +0200

    vr hand input ui fixed

--- Assets/HandleHandUIInput.cs
+++ Assets/HandleHandUIInput.cs
@@ -8,17 +8,13 @@ public class HandleHandUIInput : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void OnTriggerEnter(Collider other)
     {
-        if ((_handMask.value & (1 << other.transform.gameObject.layer)) > 0)
-        {
-            _handUIBTN.targetGraphic.color = _handUIBTN.colors.pressedColor;
-            _handUIBTN.onClick?.Invoke();
-        }
+        _handUIBTN.targetGraphic.color = _handUIBTN.colors.pressedColor;
+        _handUIBTN.onClick?.Invoke();
+        Debug.Log("clicked pressed");
     }
     private void OnTriggerExit(Collider other)
     {
-        if ((_handMask.value & (1 << other.transform.gameObject.layer)) > 0)
-        {
+        //if ((_handMask.value & (1 << other.transform.gameObject.layer)) > 0)
             _handUIBTN.targetGraphic.color = _handUIBTN.colors.highlightedColor;
-        }
     }
 }
